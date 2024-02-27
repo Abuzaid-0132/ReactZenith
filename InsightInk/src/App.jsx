dec35deb3313c5ce2fdc18526f0e63f13ac66ae0
Abuzaid-0132 <mohdabuzaid0132@gmail.com>
2024-02-27 18:54:05 +0530

    Merge remote-tracking branch 'origin/main'

--- InsightInk/src/App.jsx
+++ InsightInk/src/App.jsx
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Appwrite!</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
